7021746cf2f8863227351ebb682408b736b7796d
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-04-24 18:37:23 -0500

    Reducers
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/reducers/assets.js
@@ -0,0 +1,34 @@
+import Constants from '../common/constants'
+
+const defaultState = {
+  isLoading: true
+}
+
+const assets = (state = defaultState, action) => {
+  switch(action.type) {
+    case Constants.ACTIONS.FETCHING_ASSETS:
+      return { 
+        ...state, 
+        isLoading: true 
+      }
+
+    case Constants.ACTIONS.RECEIVING_ASSETS:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload
+      }
+
+    case Constants.ACTIONS.SET_CURRENT_ASSET:
+      return {
+        ...state,
+        isLoading: false,
+        current: action.payload
+      }
+
+    default:
+      return state
+  }
+}
+
+export default assets
