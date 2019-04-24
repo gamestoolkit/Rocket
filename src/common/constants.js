3d9d8cd7d741b9c466de5309afd5276aa5400a1d
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-04-24 18:36:51 -0500

    Create constants.js
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/common/constants.js
@@ -0,0 +1,13 @@
+
+const Constants = Object.freeze({
+  API_URL: 'https://gamestoolkitbackend.azurewebsites.net/assets',
+  API_DEV_URL: 'http://localhost:3001/assets',
+  DISQUS_SHORTNAME: 'gamestoolkit',
+  ACTIONS: {
+    FETCHING_ASSETS: 'FETCHING_ASSETS',
+    SET_CURRENT_ASSET: 'SET_CURRENT_ASSET', 
+    RECEIVING_ASSETS: 'RECEIVING_ASSETS'
+  }
+});
+
+export default Constants
