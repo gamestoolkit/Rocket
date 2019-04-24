cb73cb905742e4dd0308c6b79cfbd9aab0b32249
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-04-24 18:41:35 -0500

    Assets tests
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/components/Assets.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Assets from './Assets'
+
+jest.mock('./Asset', () => 'Asset');
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Assets assets={[]} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('fails when no list is given', () => {
+  expect(() => renderer
+      .create(<Assets assets={{}} />)).toThrowError();
+});
