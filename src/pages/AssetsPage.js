34312c10858ca6c87c008545a9fb502c38387f1e
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-04-24 18:42:21 -0500

    Create AssetsPage.js
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/pages/AssetsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import AssetsContainer from '../containers/AssetsContainer'
+import SearchBar from '../components/SearchBar';
+
+const AssetsPage = () => ( 
+  <>
+    <AssetsContainer />
+  </>
+)
+
+export default AssetsPage
