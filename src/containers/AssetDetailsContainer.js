d7bda0eb3fe154044c87d37aad16abe812f60fb0
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-04-24 18:41:54 -0500

    Create AssetDetailsContainer.js
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/containers/AssetDetailsContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { fetchCurrentAsset } from "../actions";
+import { getCurrentAsset, getIsLoading, getUrlSlug } from "../selectors";
+import { connect } from "react-redux";
+import AssetDetails from "../components/AssetDetails";
+import { withRouter } from 'react-router'
+
+class AssetDetailsContainer extends React.Component {
+  fetchData() {
+    this.props.fetchCurrentAsset(this.props.urlSlug)
+  }
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.urlSlug !== this.props.urlSlug)
+      this.fetchData()
+  }
+
+  render(){
+    if (this.props.isLoading || !this.props.asset)
+      return <h1>Loading</h1>
+    else   
+      return <AssetDetails asset={this.props.asset} />
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  urlSlug: getUrlSlug(ownProps),
+  asset: getCurrentAsset(state),
+  isLoading: getIsLoading(state)
+})
+
+export default withRouter(connect(
+  mapStateToProps,
+  { fetchCurrentAsset }
+)(AssetDetailsContainer))
