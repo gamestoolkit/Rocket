68c0c7840d83fda89bc451c4b09e2955bc59f56b
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-04-24 18:42:15 -0500

    Base files
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './overrides.css';
+import Root from './components/Root';
+import configureStore from './configureStore'
+
+const store = configureStore()
+
+ReactDOM.render(
+  <Root store={store} />, 
+  document.getElementById('root')
+)
