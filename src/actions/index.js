475e51708be35c5f45be1d4ee567142a02edfbd5
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-04-24 18:38:03 -0500

    Actions
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ src/actions/index.js
@@ -0,0 +1,37 @@
+import Constants from '../common/constants'
+import AssetsApi from '../api';
+
+export const fetchingAssets = () => ({
+  type: Constants.ACTIONS.FETCHING_ASSETS
+})
+
+export const receivingAssets = (assetsData) => ({
+  type: Constants.ACTIONS.RECEIVING_ASSETS,
+  payload: assetsData
+})
+
+export const fetchAssets = () => async(dispatch) => {
+  dispatch(fetchingAssets())
+  const api = new AssetsApi()
+  const assets = await api.findAll()
+  dispatch(receivingAssets(assets))
+}
+
+export const setCurrentAsset = (assetData) => ({
+  type: Constants.ACTIONS.SET_CURRENT_ASSET,
+  payload: assetData
+})
+
+export const fetchCurrentAsset = (urlSlug) => async(dispatch) => {
+  dispatch(fetchingAssets())
+  const api = new AssetsApi()
+  const asset = await api.findByUrlSlug(urlSlug)
+  dispatch(setCurrentAsset(asset))
+}
+
+export const filterAssets = (query) => async(dispatch) => {
+  dispatch(fetchingAssets());
+  const api = new AssetsApi();
+  const assets = await api.filter(query);
+  dispatch(receivingAssets(assets));  
+};
